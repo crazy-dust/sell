2f14872a358698c591696e309106264b529bc641
ldy <crazy_coding1102@163.com>
2019-03-25 23:24:46 +0800

    redis分布式锁-模拟秒杀场景

---
+++ src/main/java/com/demotwo/sell/service/SecKillService.java
@@ -0,0 +1,24 @@
+package com.demotwo.sell.service;
+
+/**
+ * 秒杀场景模拟
+ */
+public interface SecKillService {
+
+    /**
+     * 查询秒杀活动特价商品的信息
+     *
+     * @param productId
+     * @return
+     */
+    String querySecKillProductInfo(String productId);
+
+    /**
+     * 模拟不同用户秒杀同一商品的请求
+     *
+     * @param productId
+     * @return
+     */
+    void orderProductMockDiffUser(String productId);
+
+}
